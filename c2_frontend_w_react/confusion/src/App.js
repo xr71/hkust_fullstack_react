4be6238364e4e98625b9ec0865b951412767a179
Xu Ren <musicalxu@hotmail.com>
2020-02-01 13:14:43 +0400

    react npx init

--- c2_frontend_w_react/confusion/src/App.js
+++ c2_frontend_w_react/confusion/src/App.js
@@ -1,34 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
+import { Navbar } from 'reactstrap';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent'
-import { DISHES } from './shared/dishes'
 
+function App() {
+  return (
+    <div className="App">
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dishes: DISHES
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-
-        <Menu dishes={this.state.dishes} />
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
