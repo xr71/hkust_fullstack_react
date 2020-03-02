db91f29ee0925591ff94841251507f1e37eaa86b
Xu Ren <musicalxu@hotmail.com>
2020-03-02 22:34:59 +0800

    res endpoints project using express router

--- c4_server_side/4-intro-express/index.js
+++ c4_server_side/4-intro-express/index.js
@@ -4,6 +4,9 @@ const express = require('express'),
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
+
 
 const hostname = 'localhost';
 const port = 3000;
@@ -14,49 +17,9 @@ app.use(morgan('dev'));
 // parse application/json
 app.use(bodyParser.json());
 
-
 app.use('/dishes', dishRouter);
-
-// app.all('/dishes', (req, res, next) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-
-//   next();
-// })
-
-
-// app.get('/dishes', (req, res, next) => {
-//   res.end('We will send you all the dishes');
-// })
-
-// app.put('/dishes', (req, res, next) => {
-//   res.statusCode = 403;
-//   res.end('PUT operation not supported on /dishes');
-// });
-
-// app.delete('/dishes', (req, res, next) => {
-//   res.end('Deleting all dishes');
-// });
-
-// app.get('/dishes/:dishId', (req,res,next) => {
-//   res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-// });
-
-// app.post('/dishes/:dishId', (req, res, next) => {
-// res.statusCode = 403;
-// res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-// });
-
-// app.put('/dishes/:dishId', (req, res, next) => {
-// res.write('Updating the dish: ' + req.params.dishId + '\n');
-// res.end('Will update the dish: ' + req.body.name + 
-//       ' with details: ' + req.body.description);
-// });
-
-// app.delete('/dishes/:dishId', (req, res, next) => {
-//   res.end('Deleting dish: ' + req.params.dishId);
-// });
-
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 
