8633259dce80b34c93577b27c622fd2f1f7f5e6b
Xu Ren <musicalxu@hotmail.com>
2020-03-05 10:53:03 +0800

    callback implementation

--- c4_server_side/6-mongo-mongoose/index.js
+++ c4_server_side/6-mongo-mongoose/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 // const url = 'mongodb://localhost:27017/';
 const url = 'mongodb://192.168.99.100:27017';
@@ -12,24 +13,42 @@ MongoClient.connect(url, (err, client) => {
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
-    (err, result) => {
-        assert.equal(err,null);
+    // const collection = db.collection("dishes");
+    // collection.insertOne({"name": "Uthappizza", "description": "test"},
+    // (err, result) => {
+    //     assert.equal(err,null);
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+    //     console.log("After Insert:\n");
+    //     console.log(result.ops);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
+    //     collection.find({}).toArray((err, docs) => {
+    //         assert.equal(err,null);
             
-            console.log("Found:\n");
-            console.log(docs);
+    //         console.log("Found:\n");
+    //         console.log(docs);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+    //         db.dropCollection("dishes", (err, result) => {
+    //             assert.equal(err,null);
 
-                client.close();
+    //             client.close();
+    //         });
+    //     });
+    // });
+
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test" }, 'dishes', (result) => {
+        console.log(`Insert document ${result.ops}`);
+
+        dboper.findDocuments(db, 'dishes', (docs) => {
+            console.log(`Found documents ${docs}`);
+
+            dboper.updateDocument(db, { name: 'Vadonut' }, { description: 'Updated Test' }, 'dishes', (result) => {
+                console.log(`Updated document\n`, result.result);
+
+                db.dropCollection('dishes', (result) => {
+                    console.log(`Droped ${result}`);
+
+                    client.close();
+                });
             });
         });
     });
