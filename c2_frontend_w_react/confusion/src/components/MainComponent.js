f9557e5b32d5cce5f8eff3c0add1da5e09cad5e0
Xu Ren <musicalxu@hotmail.com>
2020-03-19 00:01:27 +0800

    About Component

--- c2_frontend_w_react/confusion/src/components/MainComponent.js
+++ c2_frontend_w_react/confusion/src/components/MainComponent.js
@@ -5,11 +5,10 @@ import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 import { COMMENTS } from '../shared/comments';
 
-import DishDetail from './DishDetailComponent';
-
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
@@ -61,6 +60,7 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route exact path='/contactus' component={Contact} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
               <Redirect to="/home" />
             </Switch>
         
