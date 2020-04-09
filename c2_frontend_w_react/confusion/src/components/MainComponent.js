511e02a6f1810e026fa2f62645ff45cde40c7d19
Xu Ren <musicalxu@hotmail.com>
2020-04-09 17:19:07 +0800

    add thunk and logger

--- c2_frontend_w_react/confusion/src/components/MainComponent.js
+++ c2_frontend_w_react/confusion/src/components/MainComponent.js
@@ -1,51 +1,38 @@
-import React, {
-  Component
-} from 'react';
-import Menu from './MenuComponent';
+import React, { Component } from "react";
+import Menu from "./MenuComponent";
 
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import Contact from './ContactComponent';
-import About from './AboutComponent';
-import DishDetail from './DishDetailComponent';
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import Contact from "./ContactComponent";
+import About from "./AboutComponent";
+import DishDetail from "./DishDetailComponent";
 
-import Home from './HomeComponent';
-import {
-  Switch,
-  Route,
-  Redirect,
-  withRouter
-} from 'react-router-dom';
-import {
-  connect
-} from 'react-redux';
+import Home from "./HomeComponent";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
-import {
-  addComment
-} from '../redux/ActionCreators';
+import { addComment } from "../redux/ActionCreators";
 
 const mapStoreToProps = (state) => {
   return {
     dishes: state.dishes,
     leaders: state.leaders,
     comments: state.comments,
-    promotions: state.promotions
-  }
-}
+    promotions: state.promotions,
+  };
+};
 
-const mapDispatchToProps = dispatch => ({
-
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
-
+const mapDispatchToProps = (dispatch) => ({
+  addComment: (dishId, rating, author, comment) =>
+    dispatch(addComment(dishId, rating, author, comment)),
 });
 
 class Main extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
-      // selectedDish: null 
+      // selectedDish: null
     };
   }
 
@@ -56,91 +43,57 @@ class Main extends Component {
   // }
 
   render() {
+    const HomePage = () => {
+      return (
+        <Home
+          dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
 
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.props.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.props.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+          addComment={this.props.addComment}
+        />
+      );
+    };
 
-      const HomePage = () => {
-        return ( <
-          Home dish = {
-            this.props.dishes.filter((dish) => dish.featured)[0]
-          }
-          promotion = {
-            this.props.promotions.filter((promo) => promo.featured)[0]
-          }
-          leader = {
-            this.props.leaders.filter((leader) => leader.featured)[0]
-          }
+    return (
+      <div className='App'>
+        <Header />
+
+        <Switch>
+          <Route path='/home' component={HomePage} />{" "}
+          <Route
+            exact
+            path='/menu'
+            component={() => <Menu dishes={this.props.dishes} />}
           />
-        )
-      }
-
-
-      const DishWithId = ({
-        match
-      }) => {
-        return ( <
-          DishDetail dish = {
-            this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]
-          }
-          comments = {
-            this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))
-          }
-          addComment = {
-            this.props.addComment
-          }
+          <Route path='/menu/:dishId' component={DishWithId} />{" "}
+          <Route exact path='/contactus' component={Contact} />{" "}
+          <Route
+            exact
+            path='/aboutus'
+            component={() => <About leaders={this.props.leaders} />}
           />
+          <Redirect to='/home' />
+        </Switch>
 
-        );
+        <Footer />
+      </div>
+    );
+  }
+}
 
-      };
-
-      return ( < div className = "App" >
-        <
-        Header / >
-
-
-        <
-        Switch >
-        <
-        Route path = '/home'
-        component = {
-          HomePage
-        }
-        /> <
-        Route exact path = '/menu'
-        component = {
-          () => < Menu dishes = {
-            this.props.dishes
-          }
-          />} / >
-          <
-          Route path = '/menu/:dishId'
-          component = {
-            DishWithId
-          }
-          /> <
-          Route exact path = '/contactus'
-          component = {
-            Contact
-          }
-          /> <
-          Route exact path = '/aboutus'
-          component = {
-            () => < About leaders = {
-              this.props.leaders
-            }
-            />} / >
-            <
-            Redirect to = "/home" / >
-            <
-            /Switch>
-
-            <
-            Footer / >
-
-            <
-            /div>
-          );
-        }
-      }
-
-      export default withRouter(connect(mapStoreToProps, mapDispatchToProps)(Main));
+export default withRouter(connect(mapStoreToProps, mapDispatchToProps)(Main));
--- c2_frontend_w_react/confusion/src/redux/configureStore.js
+++ c2_frontend_w_react/confusion/src/redux/configureStore.js
@@ -1,30 +1,21 @@
-import {
-    createStore,
-    combineReducers
-} from 'redux';
-import {
-    Dishes
-} from './dishes';
-import {
-    Comments
-} from './comments';
-import {
-    Leaders
-} from './leaders';
-import {
-    Promotions
-} from './promotions';
-
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Leaders } from "./leaders";
+import { Promotions } from "./promotions";
+import { thunk } from "redux-thunk";
+import { logger } from "redux-logger";
 
 export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
-            dishes: Dishes,
-            comments: Comments,
-            promotions: Promotions,
-            leaders: Leaders
-        })
-    );
+  const store = createStore(
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders,
+    }),
+    applyMiddleware(thunk, logger)
+  );
 
-    return store;
-}
+  return store;
+};
