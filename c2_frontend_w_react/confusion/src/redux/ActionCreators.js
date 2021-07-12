11f246c755c3f7d54f4ae62f3120eebb4363077b
Xu Ren <musicalxu@hotmail.com>
2021-07-12 09:54:09 +0800

    add json-server files

--- c2_frontend_w_react/confusion/src/redux/ActionCreators.js
+++ c2_frontend_w_react/confusion/src/redux/ActionCreators.js
@@ -1,5 +1,6 @@
 import * as ActionTypes from "./ActionTypes";
 import { DISHES } from "../shared/dishes";
+import { baseUrl } from '../shared/baseUrl'
 
 export const addComment = (dishId, rating, author, comment) => ({
   type: ActionTypes.ADD_COMMENT,
@@ -14,9 +15,9 @@ export const addComment = (dishId, rating, author, comment) => ({
 export const fetchDishes = () => (dispatch) => {
   dispatch(dishesLoading(true));
 
-  setTimeout(() => {
-    dispatch(addDishes(DISHES));
-  }, 2000);
+    return fetch(baseUrl + 'dishes')
+        .then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)))
 };
 
 export const dishesLoading = () => ({
@@ -31,4 +32,15 @@ export const dishFailed = (errmsg) => ({
 export const addDishes = (dishes) => ({
   type: ActionTypes.ADD_DISHES,
   payload: dishes,
+});
+
+export const fetchComments = () => (dispatch) => {
+    return fetch(baseUrl + 'comments')
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)))
+};
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments,
 });
--- c2_frontend_w_react/confusion/src/shared/comments.js
+++ c2_frontend_w_react/confusion/src/shared/comments.js
@@ -1,4 +1,4 @@
-export const COMMENTS = 
+export const COMMENTS =
 [
     {
         id: 0,
